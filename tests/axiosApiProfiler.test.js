455a0e7de90bc9d76afd8acf957f1696ca37895e
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-04-20 23:05:12 +0530

    added test scenarios

--- tests/axiosApiProfiler.test.js
+++ tests/axiosApiProfiler.test.js
@@ -1,4 +1,15 @@
+import APIRegister from '../src/apiRegister';
+
+jest.mock('../src/apiRegister', () => {
+	return jest.fn().mockImplementation(() => {
+		return { getRegister: jest.fn() };
+	});
+});
 import axiosRequestProfiler, { getAPIProfileData, responseProfilingCollector, requestProfilingMarker } from '../src/index';
+
+beforeEach(() => {
+	APIRegister.mockClear();
+});
 
 describe('axios interceptor - api profiler', () => {
 	test('should add itself in interceptor chain of axios', () => {
@@ -16,4 +27,16 @@ describe('axios interceptor - api profiler', () => {
 		expect(axiosMock.interceptors.request.use).toBeCalledWith(requestProfilingMarker);
 		expect(axiosMock.interceptors.response.use).toBeCalledWith(responseProfilingCollector);
 	});
+
+	test('should get all API Profiling data', () => {});
+
+	test('should mark an api for profiling', () => {});
+
+	test('should not mark an api for profiling if ignoreApiProfiling flag used', () => {});
+
+	test('should complete api profile for an api', () => {});
+
+	test('should not complete api profile for an api if ignoreApiProfilingLogs flag used', () => {});
+
+	test('should complete api profile for an api but not log the details if ignoreApiProfilingLogs flag used', () => {});
 });
