c52843bf43e3b6ebc5819d1a3c73575874e6dc80
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-04-20 23:22:46 +0530

    changed to classes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
 import APIRegister from './apiRegister';
 
-const registerInstance = new APIRegister();
+export class APIProfiler {
+	constructor() {
+		this.registerInstance = new APIRegister();
+	}
+	getAPIProfileData() {
+		return this.registerInstance.getRegister();
+	}
 
-export function getAPIProfileData() {
-	return registerInstance.getRegister();
-}
+	responseProfilingCollector(axiosConfig) {
+		if (!axiosConfig.config.ignoreApiProfiling) {
+			const profilingData = this.registerInstance.completeApiProfiling(axiosConfig);
 
-export function responseProfilingCollector(axiosConfig) {
-	if (!axiosConfig.config.ignoreApiProfiling) {
-		const profilingData = registerInstance.completeApiProfiling(axiosConfig);
-
-		if (!axiosConfig.config.ignoreApiProfilingLogs) {
-			console.log('%c Request Profile:', 'color: #4CAF50; font-weight: bold', ` ${profilingData.config.config.url} [${profilingData.config.request.status}] took ${profilingData['endTime'] - profilingData['startTime']} ms`);
+			if (!axiosConfig.config.ignoreApiProfilingLogs) {
+				console.log('%c Request Profile:', 'color: #4CAF50; font-weight: bold', ` ${profilingData.config.config.url} [${profilingData.config.request.status}] took ${profilingData['endTime'] - profilingData['startTime']} ms`);
+			}
 		}
+
+		return axiosConfig;
 	}
 
-	return axiosConfig;
-}
+	requestProfilingMarker(axiosConfig) {
+		if (!axiosConfig.ignoreApiProfiling) {
+			this.registerInstance.addAPIForProfiling(axiosConfig);
+		}
 
-export function requestProfilingMarker(axiosConfig) {
-	if (!axiosConfig.ignoreApiProfiling) {
-		registerInstance.addAPIForProfiling(axiosConfig);
+		return axiosConfig;
 	}
-
-	return axiosConfig;
 }
+
+const defaultApiProfilerInstance = new APIProfiler();
 
 export default function axiosRequestProfiler(axios) {
-	axios.interceptors.request.use(requestProfilingMarker);
-	axios.interceptors.response.use(responseProfilingCollector);
+	axios.interceptors.request.use(defaultApiProfilerInstance.requestProfilingMarker);
+	axios.interceptors.response.use(defaultApiProfilerInstance.responseProfilingCollector);
 }
